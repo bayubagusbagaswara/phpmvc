b9f687597c00f6feeb64dcd1e4b4da83c73882f0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2020-09-08 14:24:08 +0700

    menambahkan fitur Update Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -9,7 +9,7 @@
         <div class="col-lg-6">
             <!-- tombol -->
             <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+            <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
             <br><br>
@@ -17,42 +17,39 @@
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
                     <li class="list-group-item "><?= $mhs['nama']; ?>
-                        <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-pill badge-danger float-right ml-1" onclick="return confirm('yakin?')">hapus</a>
-                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-pill badge-primary float-right ml-1">detail</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-pill badge-danger float-right" onclick="return confirm('yakin?')">hapus</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-pill badge-success float-right tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">ubah</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-pill badge-primary float-right">detail</a>
                     </li>
-
                 <?php endforeach; ?>
             </ul>
-
-
         </div>
     </div>
-
-
 </div>
 <!-- Modal -->
 <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+                    <input type="hidden" name="id" id="id">
                     <div class="form-group">
                         <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" name="nama">
+                        <input type="text" class="form-control" id="nama" name="nama" autocomplete="off">
                     </div>
                     <div class="form-group">
                         <label for="nrp">NRP</label>
-                        <input type="number" class="form-control" id="nrp" name="nrp">
+                        <input type="number" class="form-control" id="nrp" name="nrp" autocomplete="off">
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="email@example.com">
                     </div>
                     <div class="form-group">
                         <label for="jurusan">Jurusan</label>
@@ -61,7 +58,6 @@
                             <option value="Teknik Elektro">Teknik Elektro</option>
                             <option value="Teknik Mesin">Teknik Mesin</option>
                             <option value="Teknik Sipil">Teknik Sipil</option>
-
                         </select>
                     </div>
             </div>
