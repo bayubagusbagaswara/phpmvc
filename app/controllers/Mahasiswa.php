611c20bf0093cd74b49dfdd9b5346b4a1cc32a70
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2020-09-08 13:04:28 +0700

    membuat Insert Data, Flash Message, dan Delete Data

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -10,4 +10,40 @@ class Mahasiswa extends Controller
         $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
+
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+            // set flash message
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+    public function hapus($id)
+    {
+        if ($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0) {
+            // set flash message
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
 }
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -18,4 +18,38 @@ class Mahasiswa_model
         $this->db->query('SELECT * FROM ' . $this->table);
         return $this->db->resultSet();
     }
+
+    public function getMahasiswaById($id)
+    {
+        // untuk menyimpan data yang akan dibinding, jadi id nya tidak langsung dimasukkin ke $id untuk menghindari SQL Injection
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        // id nya baru dimasukkin
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query  = "INSERT INTO mahasiswa
+        VALUES 
+        ('', :nama, :nrp, :email, :jurusan)";
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        // tinggal di eksekusi
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+    public function hapusDataMahasiswa($id)
+    {
+        $query = "DELETE FROM mahasiswa WHERE id =:id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
 }
